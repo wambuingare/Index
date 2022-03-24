9d029b301a21872db1358d0eb741df955bb0e962
Wambuingare <wambuinancyngare@gmail.com>
2022-03-24 08:54:51 +0300

    Elements

---
+++ src/main.kt
@@ -0,0 +1,15 @@
+
+fun main (){
+    fruit()
+    continent()
+}
+
+fun fruit(){
+    var name ="Banana"
+ println(name[2].toString()+name[3]+name[4])
+}
+
+fun continent(){
+    var place = "Antartica"
+    println(place[2].toString()+place[4]+place[6])
+}
